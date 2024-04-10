7e76961386c367c3adcfe829f8d63035433abfaf
Trafexofive <les5phenix@gmail.com>
2024-04-10 10:37:21 +0000

    m

---
+++ lua/mlamkadm/core/init.lua
@@ -0,0 +1,1 @@
+require("mlamkadm.core.options")
---
+++ lua/mlamkadm/core/options.lua
@@ -0,0 +1,23 @@
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.number = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline= true
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+vim.opt.backspace = "indent,eol,start"
+
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+
