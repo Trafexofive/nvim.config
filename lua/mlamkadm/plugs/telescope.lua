4dcf531db4c082e5dfd4784b64f07f5f759d26cb
Trafexofive <les5phenix@gmail.com>
2024-04-17 16:59:08 +0100

    gitfiles telescope keybind

--- lua/mlamkadm/plugs/telescope.lua
+++ lua/mlamkadm/plugs/telescope.lua
@@ -1,10 +1,10 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  dependencies = {'nvim-lua/plenary.nvim'},
-  keys = {
-    {'<leader><leader>', '<cmd>Telescope find_files<cr>'},
-    {'<leader>b', '<cmd>Telescope buffers<cr>'},
-  },
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { '<leader><leader>', '<cmd>Telescope find_files<cr>' },
+        { '<leader>b',        '<cmd>Telescope buffers<cr>' },
+        { '<leader>i',        '<cmd>Telescope git_files<cr>' },
+    },
 }
-
