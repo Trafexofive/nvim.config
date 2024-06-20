347068da801b172d6dd6ee7607fa5dd614bdbcc5
Trafexofive <les5phenix@gmail.com>
2024-06-20 19:40:55 +0100

    lapptop commit

--- lua/mlamkadm/core/options.lua
+++ lua/mlamkadm/core/options.lua
@@ -20,3 +20,25 @@ vim.opt.backspace = "indent,eol,start"
 -- Example using a list of specs with the default options
 vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+
+if vim.fn.executable('wl-copy') == 1 then
+  vim.g.clipboard = {
+    name = 'wl-clipboard',
+    copy = {
+      ['+'] = 'wl-copy',
+      ['*'] = 'wl-copy',
+    },
+    paste = {
+      ['+'] = 'wl-paste',
+      ['*'] = 'wl-paste',
+    },
+    cache_enabled = 1,
+  }
+
+  -- Yank to system clipboard
+  vim.api.nvim_set_keymap('n', 'y', '"+y', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 'yy', '"+yy', { noremap = true, silent = true })
+end
+
