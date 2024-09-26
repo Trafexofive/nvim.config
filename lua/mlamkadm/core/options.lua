468299f3922ea66a87e3dd9d476df0c93504ce39
Trafexofive <les5phenix@gmail.com>
2024-09-27 00:01:31 +0100

    latest

--- lua/mlamkadm/core/options.lua
+++ lua/mlamkadm/core/options.lua
@@ -4,6 +4,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 --vim.opt.undo_history = true
 
 vim.opt.ignorecase = true
